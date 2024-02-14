1033721510af3d52b3fc16ba55b4cc8a9f15c761
Tsvetozara <tsvetozara.beneva@gmail.com>
2024-02-14 13:52:04 +0200

    toString() method example exercise code

---
+++ src/toStringmethod/Main.java
@@ -0,0 +1,14 @@
+package toStringmethod;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //toString() = special method that all objects  inherit,
+        //             that returns  a string that "textually represents" an object
+
+        Car car = new Car();
+
+        System.out.println(car);
+    }
+}
