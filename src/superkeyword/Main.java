bdaf72883f53c5a081001b893ec73183f1e761eb
Tsvetozara <tsvetozara.beneva@gmail.com>
2024-02-15 15:29:58 +0200

    Use of super keyword example code

---
+++ src/superkeyword/Main.java
@@ -0,0 +1,12 @@
+package superkeyword;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //super keyword refers to the superclass (parent) of an object
+
+        Hero hero1 = new Hero("Superman", 43, "Everything");
+
+        System.out.println(hero1);
+    }
+}
